4f47e2aca29e42ec899eb5ace466c12b530223b7
Antoine Duval <antoine@cuffel.fr>
2022-08-21 22:57:17 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ class Spell extends LIBRARIES.Skill {
     const SELF = this;
 
     this.Main.Manager.addAction("Spell.spellAWord", function(_intent, _socket){
-      _intent.Variables.text = _intent.Variables.word.split("").join(" ").toUpperCase();
+      _intent.Variables.text = _intent.Variables.word.split("").join(",").toUpperCase();
       _intent.answer(_socket);
     });
   }
